4576ee73eb553fdb9b051f44f5aa3a7ea11aedf8
Michael Kofi Awuni <michaelawunikofi@gmail.com>
2026-02-02 02:26:56 +0000

    feat: add optional return to allow return of none for invalid inputs

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -11,24 +11,29 @@ fn main() {
         .read_line(&mut number)
         .expect("Enter a valid numver");
     
-    println!("You just typed : {}", number);
     let number: i64 = number
             .trim()
             .parse()
             .expect("Invalid number received");
+
     println!("Value after parsing is: {}", number);
-    println!("The {}th Fibonacci number is: {}", number, fib(number));
+    println!("The {}th Fibonacci number is: {:#?}", number, fib(number));
 }
 
 // TODO: Find out if there's a way to include optional return like in js
-fn fib(n: i64) -> i64 {
+fn fib(n: i64) -> Option<i64> {
     if n < 0 {
-        return 0;
+        return None;
     }
 
-    return match n {
-        0 => 0,
-        1 => 1,
-        _ => fib(n-1) + fib(n-2),
+    match n {
+        0 => Some(0),
+        1 => Some(1),
+        _ => {
+            match (fib(n-1), fib(n-2)) {
+                (Some(a), Some(b)) => Some(a+b),
+                _ => None,
+            }
+        }
     };
 }
